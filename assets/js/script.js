28b0a4282767e439fbe41541b206b8c268d83481
MandyHole <mandyhole17@gmail.com>
2022-09-16 15:58:13 +0000

    Finish moving footballQuesions array from json back to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,374 @@ const footballQuestions =
    c: "23 years",
     d: "26 years",
     correct: "26 years"
+},
+{
+    id: 6,
+    question: "If a player distracts an opponent whilst taking a throw-in, what decision can the referee give?",
+    a: "Yellow card",
+    b: "Red card",
+    c: "Direct free kick",
+    d: "Indirect free kick",
+    correct: "Yellow card"
+}, {
+    id: 7,
+    question: "In 2018-2019, three Premier League players shared the Golden Boot, each scoring 22 goals. Which of these players did NOT receive the award that year, having scored 21 goals?",
+    a: "Mohamed Salah",
+    b: "Sergio Aguero",
+    c: "Pierre-Emerick Aubameyang",
+    d: "Sadio Mane",
+    correct: "Sergio Aguero"
+}, {
+    id: 8,
+    question: "The 2015 Super Cup saw the most goals in the history of the competition; how many were scored?",
+    a: "8",
+    b: "9",
+    c: "10",
+    d: "11",
+    correct: "9"
+}, {
+    id: 9,
+    question: "In the 1974 World Cup, which British team were knocked out on goal difference, but were the only unbeaten side in the tournament?",
+    a: "England",
+    b: "Republic of Ireland",
+    c: "Scotland",
+    d: "Wales",
+    correct: "Scotland"
+}, {
+    id: 10,
+    question: "What is the name of Fulham’s football stadium?",
+    a: "Craven Cottage",
+    b: "Carrow Road",
+    c: "Fratton Park",
+    d: "St James Park",
+    correct: "Craven Cottage"
+}, {
+    id: 11,
+    question: "Which football club play at the Bernabeu Stadium?",
+    a: "Barcelona FC",
+    b: "Real Madrid FC",
+    c: "Athletic Bilbao",
+    d: "Atlético Madrid",
+    correct: "Real Madrid FC"
+}, {
+    id: 12,
+    question: "Which country hosted the 1990 World Cup?",
+    a: "Mexico",
+    b: "Italy",
+    c: "United States",
+    d: "France",
+    correct: "Italy"
+}, {
+    id: 13,
+    question: "What are the dimensions of an adult football goal?",
+    a: "9ft x 23ft",
+    b: "10ft x 23ft",
+    c: "9ft x 24ft",
+    d: "8ft x 24ft",
+    correct: "8ft x 24ft"
+}, {
+    id: 14,
+    question: "Which two players scored for England in the 1998 World Cup match versus Colombia?",
+    a: "Darren Anderton and David Beckham",
+    b: "Michael Owen and Alan Shearer",
+    c: "Michael Owen and Darren Anderton",
+    d: "Alan Shearer and David Beckham",
+    correct: "Darren Anderton and David Beckham"
+}, {
+    id: 15,
+    question: "Which club plays at Goodison Park?",
+    a: "Reading",
+    b: "Leicester",
+    c: "Everton",
+    d: "Liverpool",
+    correct: "Everton"
+}, {
+    id: 16,
+    question: "How far must the wall be away from a direct free kick (in yards)?",
+    a: "5 yards",
+    b: "10 yards",
+    c: "12 yards",
+    d: "15 yards",
+    correct: "10 yards"
+}, {
+    id: 17,
+    question: "Who is the youngest player ever to score a goal at the World Cup finals (as of summer 2022)?",
+    a: "Norman Whiteside",
+    b: "Samuel Eto",
+    c: "Salomon Olembé",
+    d: "Pelé",
+    correct: "Pelé"
+}, {
+    id: 18,
+    question: "What is the maximum weight of a football at the start of a game, under FIFA regulations?",
+    a: "16 ounces",
+    b: "18 ounces",
+    c: "20 ounces",
+    d: "12 ounces",
+    correct: "16 ounces"
+}, {
+    id: 19,
+    question: "Who is the first English footballer to have scored in three consecutive World Cups?",
+    a: "Wayne Rooney",
+    b: "David Beckham",
+    c: "Bobby Charlton",
+    d: "Rio Ferdinand",
+    correct: "David Beckham"
+},
+{
+    id: 20,
+    question: "Who is Arsenal’s all-time leading goal scorer (as of summer 2022)?",
+    a: "Ian Wright",
+    b: "Cliff Bastin",
+    c: "Thierry Henry",
+    d: "John Radford",
+    correct: "Thierry Henry"
+},
+{
+    id: 21,
+    question: "In which year did all four British teams qualify for the World Cup?",
+    a: "1958",
+    b: "1962",
+    c: "1966",
+    d: "1970",
+    correct: "1958"
+}, {
+    id: 22,
+    question: "What year did Maradona have his ‘Hand of God’ goal?",
+    a: "1978",
+    b: "1982",
+    c: "1986",
+    d: "1990",
+    correct: "1986"
+}, {
+    id: 23,
+    question: "Which team won fourth place at the 1994 World Cup?",
+    a: "Bulgaria",
+    b: "Italy",
+    c: "Romania",
+    d: "Sweden",
+    correct: "Bulgaria"
+}, {
+    id: 24,
+    question: "Which South American team won the 2021 Copa América?",
+    a: "Argentina",
+    b: "Brazil",
+    c: "Chile",
+    d: "Uruguay",
+    correct: "Argentina"
+}, {
+    id: 25,
+    question: "Which club has won the most Premier League titles (as of summer 2022)?",
+    a: "Arsenal",
+    b: "Everton",
+    c: "Liverpool",
+    d: "Manchester United",
+    correct: "Manchester United"
+}, {
+    id: 26,
+    question: "Which was the top club in 2020/2021 based on total revenue (644.9 million Euro)?",
+    a: "Bayern Munich",
+    b: "FC Barcelona",
+    c: "Manchester City",
+    d: "Real Madrid",
+    correct: "Manchester City"
+}, {
+    id: 27,
+    question: "Which team has the most consecutive UEFA Champions League match victories (as of summer 2022)?",
+    a: "Barcelona",
+    b: "Bayern Munich",
+    c: "Manchester City",
+    d: "Real Madrid",
+    correct: "Bayern Munich"
+}, {
+    id: 28,
+    question: "In which country would you find La Liga?",
+    a: "Italy",
+    b: "Mexico",
+    c: "Portugal",
+    d: "Spain",
+    correct: "Spain"
+}, {
+    id: 29,
+    question: "Which manager was awarded Premier Manager of the Month with four different clubs?",
+    a: "Harry Redknapp",
+    b: "David Moyes",
+    c: "Roy Hodgson",
+    d: "José Mourinho",
+    correct: "Harry Redknapp"
+},
+{
+    id: 30,
+    question: "What year did Wayne Rooney score his 50th international goal?",
+    a: "2013",
+    b: "2014",
+    c: "2015",
+    d: "2016",
+    correct: "2015"
+},
+{
+    id: 31,
+    question: "What is the minimum length of a football pitch?",
+    a: "100 yards",
+    b: "110 yards",
+    c: "120 yards",
+    d: "130 yards",
+    correct: "100 yards"
+}, {
+    id: 32,
+    question: "Alan Shearer was the top goal scorer in the Euro ’96 finals; how many goals did he score?",
+    a: "4",
+    b: "5",
+    c: "6",
+    d: "7",
+    correct: "5"
+}, {
+    id: 33,
+    question: "Which year did Phil Neville leave Manchester United to join Everton?",
+    a: "2002",
+    b: "2003",
+    c: "2004",
+    d: "2005",
+    correct: "2005"
+}, {
+    id: 34,
+    question: "In which year was the number of teams competing in a World Cup increased to 32?",
+    a: "1990",
+    b: "1994",
+    c: "1998",
+    d: "2002",
+    correct: "1998"
+}, {
+    id: 35,
+    question: "What position did Sir Alex Ferguson play during his football career?",
+    a: "Goalie",
+    b: "Defender",
+    c: "Midfielder",
+    d: "Striker",
+    correct: "Striker"
+}, {
+    id: 36,
+    question: "A match may not start or continue if either team has fewer than how many players?",
+    a: "6 players",
+    b: "7 players",
+    c: "8 players",
+    d: "9 players",
+    correct: "7 players"
+}, {
+    id: 37,
+    question: "Which Chelsea player scored the most goals in the Champions League (as of summer 2022)?",
+    a: "Didier Drogba",
+    b: "Frank Lampard",
+    c: "John Terry",
+    d: "Fernando Torres",
+    correct: "Didier Drogba"
+}, {
+    id: 38,
+    question: "How many England managers were there between 1947 and 1973?",
+    a: "2",
+    b: "3",
+    c: "4",
+    d: "5",
+    correct: "2"
+}, {
+    id: 39,
+    question: "Three teams have had eight FA Cup final defeats (as of summer 2022). Which one of these teams does not share this record?",
+    a: "Chelsea",
+    b: "Everton",
+    c: "Liverpool",
+    d: "Manchester United",
+    correct: "Liverpool"
+},
+{
+    id: 40,
+    question: "How many players were sent off during the Battle of Santiago between Italy and Chile in 1962?",
+    a: "2",
+    b: "4",
+    c: "6",
+    d: "8",
+    correct: "2"
+},
+{
+    id: 41,
+    question: "When did Pelé retire?",
+    a: "1969",
+    b: "1971",
+    c: "1974",
+    d: "1976",
+    correct: "1974"
+}, {
+    id: 42,
+    question: "Which football position requires the most running (based on an average distance covered of 11.2km)?",
+    a: "Central Midfielders",
+    b: "Full-Backs",
+    c: "Strikers",
+    d: "Wingers",
+    correct: "Central Midfielders"
+}, {
+    id: 43,
+    question: "What is the most common score line in a Premier League football match (2015/16 to 2019/20)?",
+    a: "0-0",
+    b: "1-0",
+    c: "1-1",
+    d: "2-1",
+    correct: "1-0"
+}, {
+    id: 44,
+    question: "Which English team has the most consecutive seasons in the top flight football league?",
+    a: "Arsenal",
+    b: "Everton",
+    c: "Liverpool",
+    d: "Manchester United",
+    correct: "Arsenal"
+}, {
+    id: 45,
+    question: "Which country has the highest number of ‘FIFA World Player of the Year’ winners?",
+    a: "Argentina",
+    b: "Brazil",
+    c: "Italy",
+    d: "Portugal",
+    correct: "Brazil"
+}, {
+    id: 46,
+    question: "Who is the only player to have won a trophy with both Liverpool and Manchester United?",
+    a: "Peter Beardsley",
+    b: "Steven Gerrard",
+    c: "Paul Ince",
+    d: "Michael Owen",
+    correct: "Michael Owen"
+}, {
+    id: 47,
+    question: "Which team has the most appearances in top flight football?",
+    a: "Arsenal",
+    b: "Aston Villa",
+    c: "Everton",
+    d: "Liverpool",
+    correct: "Everton"
+}, {
+    id: 48,
+    question: "What is the least amount of shots a team can take to win a penalty shootout?",
+    a: "2",
+    b: "3",
+    c: "4",
+    d: "5",
+    correct: "3"
+}, {
+    id: 49,
+    question: "Who won the golden boot award in the Premier League for the 2020/2021 season?",
+    a: "Bruno Fernandes",
+    b: "Harry Kane",
+    c: "Mohamed Salah",
+    d: "Jamie Vardy",
+    correct: "Harry Kane"
+},
+{
+    id: 50,
+    question: "Which footballer’s wife was nicknamed 'Wagatha Christie'?",
+    a: "Frank Lampard",
+    b: "Cristiano Ronaldo",
+    c: "Wayne Rooney",
+    d: "Jamie Vardy",
+    correct: "Wayne Rooney"
 }]
 
 /**Run the game once the DOM content is loaded */
@@ -72,7 +440,7 @@ function submitAnswer() {
     } else if (attempts > 10){
         document.getElementById("final-message").style.display = "block";
         document.getElementById("final-message").innerHTML =
-        `You have had too many attempts (more than 10): ${attempts}. <form><button type="button" onclick="${playAgain()}">Play Again</button></form>`;
+        `You have had too many attempts (more than 10): ${attempts}. <form><button type="button">Play Again</button></form>`;
         throw `Too many attempts: ${attempts}`;
     } else {
         document.getElementById("next-question-box").style.display = "block";
