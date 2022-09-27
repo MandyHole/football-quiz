4e1586603726f63867cb2036fc7cb05b2d547b62
MandyHole <mandyhole17@gmail.com>
2022-09-27 13:29:05 +0000

    Add an auto  scroll to top when user  submits  answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -445,6 +445,8 @@ function submitAnswer() {
     addAttempt();
     rulesArea.style.display = "none";
     showRulesButton.style.display = "block";
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     let attempts = parseInt(document.getElementById("attempts").textContent);
     if (attempts == 10) {
         nextQuestionArea.style.display = "none";
